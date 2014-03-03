d4d3c403691bd01b1cf63e05f0a11b6ef2d28408
Joshua Bussdieker <jbussdieker@gmail.com>
2014-03-02 21:29:05 -0500

    user_id is not a permitted attribute

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -54,6 +54,6 @@ class AccountsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def account_params
-      params.require(:account).permit(:user_id, :name, :description)
+      params.require(:account).permit(:name, :description)
     end
 end
