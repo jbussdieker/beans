1521978ed594632b102cc4e8641f444dfc0db81e
Joshua Bussdieker <jbussdieker@gmail.com>
2014-03-02 21:07:05 -0800

    Add a top nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Beans::Application.routes.draw do
   resources :accounts
 
   devise_for :users
+
+  root :to => 'accounts#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
