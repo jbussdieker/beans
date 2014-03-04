3d6a77f91a00ed2029277210b14443d45c3ae9df
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-03-03 17:35:31 -0800

    Add scopes for account types

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,6 +3,12 @@ class Account < ActiveRecord::Base
   belongs_to :account_type
   has_many :journals
 
+  scope :assets, -> { where(:account_type_id => 1) }
+  scope :liabilities, -> { where(:account_type_id => 2) }
+  scope :equity, -> { where(:account_type_id => 3) }
+  scope :income, -> { where(:account_type_id => 4) }
+  scope :expense, -> { where(:account_type_id => 5) }
+
   def to_s
     name
   end
